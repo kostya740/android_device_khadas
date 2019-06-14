ba79ade67583325c8f3d99fa7c25a47c931a6aa8
hyperion11 <degterev_alex@mail.ru>
2019-06-14 22:42:37 +0700

    +root

--- kvim/kvim.mk
+++ kvim/kvim.mk
@@ -335,8 +335,8 @@ endif
 #                              Build Config
 #
 #########################################################################
-BUILD_WITH_GAPPS_CONFIG :=false
-BUILD_WITH_ROOT_CONFIG :=false
+BUILD_WITH_GAPPS_CONFIG :=true
+BUILD_WITH_ROOT_CONFIG :=true
 
 #########################################################################
 #
